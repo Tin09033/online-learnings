9678d517dfb945c2cb1439126e1a716915437ce1
Emma Paris <eparis@atlassian.com>
2026-03-30 09:21:28 +0800

    Update copyright to 2026

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -92,7 +92,7 @@ const Footer = () => {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 MASTERTALK. All rights reserved.</p>
+          <p>&copy; 2026 MASTERTALK. All rights reserved.</p>
         </div>
       </div>
     </footer>
